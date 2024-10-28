8a9123639280369509d2d81a8c55c5e9d8e86ed3
Jonathan Bennett <jonathanbennett984@gmail.com>
2024-10-27 19:26:01 -0500

    Add X or O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Select all divs inside the game board with a specific selector (assuming they have a specific ID or class)
     const squares = document.querySelectorAll("#board div"); // Adjust selector if needed
-
+    let isX = true;
+    const GameSquares = Array(9).fill(null)
     // Loop through each square and add the 'square' class
-    squares.forEach(square => {
+    
+    squares.forEach((square, index) => {
         square.setAttribute("class","square");
+        square.addEventListener("click", function(){
+
+        if(!GameSquares[index]){
+            GameSquares[index] = isX ? "X":"O";
+            square.textContent = GameSquares[index];
+            square.setAttribute("class", `square ${GameSquares[index]}`)
+
+            isX = !isX;
+
+        }
+        });
     });
+
+
 });
